93eff195edfc96b3dd9d87bdd3380a535ef307f9
Haozhe-H <haozhehuang@outlook.com>
2023-05-09 00:14:14 -0400

    update comments in product model

--- my-app/server/models/Product.js
+++ my-app/server/models/Product.js
@@ -26,10 +26,13 @@ const watchSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  // this category property is used to allow user to search for certain watch feature, eg. classic, luxury, female...
+  // this property will not show on product cards!!
   category: {
     type: [String],
     required: true,
   },
+  // this tags propery is used to render to the product cards, to let user know some of the watch features
   tags: {
     type: [String],
     required: true,
