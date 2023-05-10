3511e5e9b3404a5d77d8cbcf4d3ffd151408e9d9
MohamedAbdullahi2 <118404373+MohamedAbdullahi2@users.noreply.github.com>
2023-05-10 19:44:49 -0400

    seeded and added reolvers and typedef

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,11 @@
 const db = require("../config/connection");
-const Watch = require("../models/Product");
-const seedData = require("./watchdata.json");
+const Product = require("../models/Product");
+const watches = require("./watchdata.json");
 
 db.once("open", async () => {
   try {
-    await Watch.deleteMany({});
-    await Watch.insertMany(seedData.watches);
+    await Product.deleteMany({});
+    await Product.insertMany(watches);
 
     console.log("Data has been seeded successfully.");
     process.exit(0);
